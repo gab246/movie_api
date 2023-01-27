af2dd79e300cc267791f4b1cabe6740a722ecf9d
gab246 <119339137+gab246@users.noreply.github.com>
2023-01-27 12:18:01 +0100

    added top 10 movies and Express GET route for endpoint '/movies'

--- index.js
+++ index.js
@@ -1,2 +1,45 @@
 const express = require('express');
 const app = express();
+
+let topMovies = [
+        {
+            title: 'Matilda'
+        },
+        {
+            title: 'The Blind Side'
+        },
+        {
+            title: 'The Princess Diaries'
+        },
+        {
+            title: 'Witness'
+        },
+        {
+            title: 'She\'s the Man'
+        },
+        {
+            title: 'Fast and Furious'
+        },
+        {
+            title: 'Spy'
+        },
+        {
+            title: 'Divergent'
+        },
+        {
+            title: 'Avengers'
+        },
+        {
+            title: 'Her'
+        }
+                
+    ];
+
+app.get('/movies', (req, res) => {
+    res.json(topMovies);
+});
+
+
+
+
+
