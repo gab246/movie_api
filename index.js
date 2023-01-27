91c7f7a220a2373726ccd124a64f96d72008e92d
gab246 <119339137+gab246@users.noreply.github.com>
2023-01-27 16:09:45 +0100

    added morgan middleware to log requests, added error handling
    This reverts commit 1dc141e84ec0e5aa4bf2d23b288cb9651e5022d8.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express'),
- morgan = require('morgan');
+    morgan = require('morgan');
 
 const app = express();
 app.use(morgan('common'));
@@ -38,8 +38,6 @@ let topMovies = [
                 
     ];
 
-
-
 app.get('/movies', (req, res) => {
     res.json(topMovies);
 });
@@ -50,10 +48,9 @@ app.get('/', (req, res) => {
 
 app.use(express.static('public'));
 
-
 app.use((err, req, res, next) =>{
     console.error(err.stack);
-    res.status(500).send('Something broke');
+    res.status(500).send('something broke');
 });
 
 app.listen(8080, () => {
