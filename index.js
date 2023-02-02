bea7b0583f6e4bebf38d5dd7bbf637e0e14ecbad
gab246 <119339137+gab246@users.noreply.github.com>
2023-02-02 10:55:55 +0100

    added Express code to route endpoints

--- index.js
+++ index.js
@@ -15,7 +15,47 @@ app.get('/', (req, res) => {
 })
 
 
+app.get('/movies', (req, res) =>{
+    res.send('Successful GET request returning data on all movies');
+            });
 
+
+app.get('/movies/:name', (req, res) =>{
+    res.send('Successful GET request returning data on a particular movie');
+            });
+
+             
+app.get('/movies/:name/:genre', (req, res) =>{
+    res.send('Successful GET request returning data about a genre of a movie);
+             });
+          
+        
+app.get('/movies/:director', (req, res) =>{
+    res.send('Successful GET request on data about a particular director');
+            });
+
+app.post('/users', (req, res) =>{
+    res.send('Created new user');
+        });
+        
+app.put('/user/:username', (req, res) =>{
+    res.send('Updated user information')
+        });
+        
+app.post('/movies/:id/:movieTitle', (req, res) =>{
+    res.send(`Movie has been added to the favourites list`);
+        });
+        
+app.delete('/user/:id/:movieTitle', (req, res) =>{
+    res.send(`Movie has been deleted from the favourite list`);
+        });
+                
+app.delete('/user/:username', (req, res) =>{
+    res.send(`User has been deleted`);
+        });
+
+    
+    
 app.use(express.static('public'));
 
 app.use((err, req, res, next) =>{
