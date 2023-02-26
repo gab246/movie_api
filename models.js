46ec2c6aa834ea0b339d0022e1c410fe286ea4e6
gab246 <119339137+gab246@users.noreply.github.com>
2023-02-26 20:51:15 +1100

    Revert "add bcrypt and hash password to user schema"
    This reverts commit 805dfcf6381bb0fcd441f2ae0d2ded8bcb8ebbbd.

--- models.js
+++ models.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
 
 let movieSchema = mongoose.Schema({
 Title: {type: String, required: true},
@@ -26,15 +25,6 @@ Birthday: Date,
 FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
 });
 
-//hashes password
-userSchema.statics.hashPassword = (password) => {
-    return bcrypt.hashSync(password, 10);
-  };
-  
-//compared hashed to paswword in DB
-  userSchema.methods.validatePassword = function(password) {
-    return bcrypt.compareSync(password, this.Password);
-  };
 
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
