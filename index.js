843f750da33396e4a5dcc0122b90a9dfcc8a4701
gab246 <119339137+gab246@users.noreply.github.com>
2023-02-28 12:56:20 +1100

    updated http delete request

--- index.js
+++ index.js
@@ -172,7 +172,7 @@ app.post('/users/:Username/movies/:MovieID', passport.authenticate('jwt', { sess
         });
        
 //delete movie from favorites
-app.post('/users/:Username/movies/:MovieID', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.delete('/users/:Username/movies/:MovieID', passport.authenticate('jwt', { session: false }), (req, res) => {
     Users.findOneAndUpdate({Username: req.params.Username}, {
         $pull: {FavoriteMovies: req.params.MovieID}
     },
