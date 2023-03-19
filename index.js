1cb95c52964082b347ca29035752579e3ecff884
gab246 <119339137+gab246@users.noreply.github.com>
2023-03-19 11:00:55 +1100

    updated CORS and allowed origins

--- index.js
+++ index.js
@@ -29,11 +29,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(morgan('combined', {stream: accessLog}));
-app.use(cors());
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-})
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://desolate-sierra-27780.herokuapp.com/'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 
 
 
