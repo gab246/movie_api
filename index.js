aa996b1fc51a509a19cc5961aad8935ce76d703a
gab246 <119339137+gab246@users.noreply.github.com>
2023-04-24 11:53:44 +1000

    added netlify api

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'https://desolate-sierra-27780.herokuapp.com/', 'http://localhost:1234'];
+let allowedOrigins = ['http://localhost:8080', 'https://desolate-sierra-27780.herokuapp.com/', 'http://localhost:1234', 'https://mooviesbymyflix.netlify.app'];
 app.use(cors({
     origin: (origin, callback) => {
         if(!origin) return callback(null, true);
