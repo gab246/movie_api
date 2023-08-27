c7b50959d081de6344e7cc3cec4e00f897615681
gab246 <119339137+gab246@users.noreply.github.com>
2023-08-27 15:02:31 +0200

    updated origins

--- index.js
+++ index.js
@@ -15,7 +15,15 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'https://desolate-sierra-27780.herokuapp.com', 'http://localhost:1234', 'https://mooviesbymyflix.netlify.app'];
+let allowedOrigins = [
+    'http://localhost:8080', 
+    'https://desolate-sierra-27780.herokuapp.com', 
+    'http://localhost:1234', 
+    'https://mooviesbymyflix.netlify.app', 
+    'http://localhost:4200', 
+    'https://gab246.github.io/myFlix-Angular-client',
+    'https://gab246.github.io',
+];
 app.use(cors({
     origin: (origin, callback) => {
         if(!origin) return callback(null, true);
